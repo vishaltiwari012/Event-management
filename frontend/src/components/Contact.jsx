b0c5811d577b8cc3939d6cee7bcce87bc0a7dd95
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-03-09 13:20:23 +0530

    latest

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://fluffy-cloak-tuna.cyclic.app/api/v1/message/send",
         {
           name,
           email,
